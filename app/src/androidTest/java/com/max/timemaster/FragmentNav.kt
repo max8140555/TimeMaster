a19b38d1369bbb4dfb50833883e4a08493b24fef
max8140555 <61774466+max8140555@users.noreply.github.com>
2020-08-10 18:08:54 +0800

    Espresso Test

---
+++ app/src/androidTest/java/com/max/timemaster/FragmentNav.kt
@@ -0,0 +1,78 @@
+package com.max.timemaster
+
+import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.google.common.base.Verify.verify
+import com.max.timemaster.util.CurrentFragmentType
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class FragmentNav {
+    @Rule
+    @JvmField
+    var activityActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun favoriteFragmentNav(){
+        activityActivityTestRule.launchActivity(Intent())
+
+        onView(withId(R.id.navigation_favorite)).perform(click()).check(matches(isDisplayed()))
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText("喜好")))
+        Thread.sleep(3000)
+    }
+
+    @Test
+    fun costFragmentNav(){
+        activityActivityTestRule.launchActivity(Intent())
+
+        onView(withId(R.id.navigation_cost)).perform(click())
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText("花費")))
+        Thread.sleep(3000)
+    }
+
+    @Test
+    fun profileFragmentNav(){
+        activityActivityTestRule.launchActivity(Intent())
+
+        onView(withId(R.id.navigation_profile)).perform(click())
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText("個人")))
+        Thread.sleep(3000)
+    }
+    @Test
+    fun calendarFragmentNav(){
+        activityActivityTestRule.launchActivity(Intent())
+
+        onView(withId(R.id.navigation_calendar)).perform(click())
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText("時間管理大師")))
+        Thread.sleep(3000)
+    }
+
+    @Test
+    fun editDateBirthday(){
+        activityActivityTestRule.launchActivity(Intent())
+
+        onView(withId(R.id.navigation_calendar)).perform(click())
+
+    }
+}
